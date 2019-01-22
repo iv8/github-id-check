187f5f81811899a9b39e22c5e4c3bcb0c87cac80
l26 <44664770+l26@users.noreply.github.com>
2019-01-22 11:20:28 +0800

    Update run.py

--- run.py
+++ run.py
@@ -12,5 +12,5 @@ for s1 in char:
     for s2 in char:
         for s3 in char:
             if 404 == requests.head('https://github.com/' + s1 + s2 + s3).status_code:
-            print(s1 + s2 + s3)
+                print(s1 + s2 + s3)
 print('------------------------------------\nEnd Find')
