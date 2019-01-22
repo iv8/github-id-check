b05fdf1a1e013fdb5df0e6f6c39e48f944f19c6f
l2u <44664770+l2u@users.noreply.github.com>
2019-01-22 09:57:24 +0800

    Update run.py

--- run.py
+++ run.py
@@ -3,10 +3,10 @@
 import requests
 
 char = 'abcdefghijklmnopqrstuvwxyz'
-print('Begin Find\n\n')
+print('Begin Find\n------------------------------------')
 for s1 in char:
     for s2 in char:
-        for s3 in char:
-            if 404 == requests.head('https://github.com/' + s1 + s2 + s3).status_code:
-                print(s1 + s2 + s3 + ', ')
-print('\n\nEnd Find')
+        #for s3 in char:
+        if 404 == requests.head('https://github.com/' + s1 + s2).status_code:
+            print(s1 + s2 + ', ')
+print('------------------------------------\nEnd Find')
